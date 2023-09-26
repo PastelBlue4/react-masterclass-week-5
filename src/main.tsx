479deeb1ecd1cb90828cac0b0f2ff056ccfd0104
Romuru <pastelblue0721@gmail.com>
2023-09-26 18:05:23 +0900

    setting : first

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { createGlobalStyle } from "styled-components";
 import "./index.css";
 import { RecoilRoot } from "recoil";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+  
+
+
+${reset}
+
+
+
+`;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RecoilRoot>
+      <GlobalStyles />
       <App />
     </RecoilRoot>
   </React.StrictMode>
